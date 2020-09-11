ae3161006a63db71dce66dd71a600501db601b41
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-11 16:07:49 +0900

    トップページ開設

---
+++ db/migrate/20200911065131_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      # t.references :user,             null: false,  foreign_key: true
+      t.string     :name,		             null: false
+      t.text       :content,             null: false
+      t.integer    :price,	             null: false
+      t.timestamps
+    end
+  end
+end
