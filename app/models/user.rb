7af754886280b8ae1d82bab1b02f540899daf143
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-15 08:54:10 +0900

    バリデーションとログアウト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数文字を使用してください' }
+    validates :real_family_name,    format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+    validates :real_first_name,     format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+    validates :reading_family_name, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナ文字を使用してください' }
+    validates :reading_first_name,  format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナ文字を使用してください' }
+    validates :birthday
+  end
 end
