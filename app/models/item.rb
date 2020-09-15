97d985d83d4c18e7e337eb47aa8a185744869ab9
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-15 17:00:49 +0900

    Itemモデルのマイグレーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
+
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :saize_category_id, presence: true
+  validates :genre_category_id, presence: true
+  validates :price, presence: true
+  validates :unit, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,47 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_914_235_645) do
-  create_table 'addresses', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.string 'postal_code', default: '', null: false
-    t.integer 'prefecture_id', null: false
-    t.string 'city', default: '', null: false
-    t.string 'address', default: '', null: false
-    t.string 'building_name', default: ''
-    t.string 'tell', default: '', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['user_id'], name: 'index_addresses_on_user_id'
+ActiveRecord::Schema.define(version: 2020_09_14_235645) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "postal_code", default: "", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "building_name", default: ""
+    t.string "tell", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
-  create_table 'items', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.string 'name', null: false
-    t.text 'content', null: false
-    t.integer 'price', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.integer "genre_category_id", null: false
+    t.integer "saize_category_id", null: false
+    t.string "unit", default: "yen", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'nickname', null: false
-    t.string 'real_family_name', null: false
-    t.string 'real_first_name', null: false
-    t.string 'reading_family_name', null: false
-    t.string 'reading_first_name', null: false
-    t.date 'birthday', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string 'unconfirmed_email'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "real_family_name", null: false
+    t.string "real_first_name", null: false
+    t.string "reading_family_name", null: false
+    t.string "reading_first_name", null: false
+    t.date "birthday", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
