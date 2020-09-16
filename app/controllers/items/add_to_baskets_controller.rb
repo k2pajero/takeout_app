9b1378fa8d2d818895402067d9bf3c28bd71dcdd
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-16 19:06:48 +0900

    basket_itemの作成

--- app/controllers/items/add_to_baskets_controller.rb
+++ app/controllers/items/add_to_baskets_controller.rb
@@ -3,5 +3,9 @@ class Items::AddToBasketsController < Items::ApplicationController
     basket = current_user.prepare_basket
     # Userモデルにprepare_basketメソッドを定義して簡略化
     # basket = current_user.basket || current_user.create_basket
+    item = Item.find(params[:item_id])
+    basket.basket_items.create!(item_id: item.id)
+    # basket_itemの作成（リレーションを組んでからでないとできない。複数形に注意）
+    # 商品を選択するだけでユーザの入力ミスはありえないのでcreate!としている
   end
 end
