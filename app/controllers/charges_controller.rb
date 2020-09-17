acafbe9b79f4a71be2c5e6a3487a80a290d20bad
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-17 23:46:17 +0900

    カート内金額を決済に反映

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,10 +1,16 @@
 class ChargesController < ApplicationController
   def create
-    # binding.pry
     Stripe.api_key = ENV['STRIPE_SECRET_KEY']
     token = params[:stripeToken]
+    item_ids = params[:item_ids].map(&:to_i)
+    # binding.pry
+    # カート内の商品id達（paramsの中の配列文字列）を数値の配列で返す
+
+    items = current_user.basket.items.where(id: item_ids)
+    # 今買い物かごに入っている商品の中から対象の商品と同じIDの商品
+    total = items.sum(:price)
     Stripe::Charge.create({
-                            amount: 999,
+                            amount: total,
                             currency: 'jpy',
                             description: 'Example charge',
                             source: token
