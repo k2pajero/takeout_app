ce266d59738d89bad2c8ed0cca61588d374c08d3
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-17 16:42:16 +0900

    合計金額のビュー表示

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,4 +1,14 @@
 class Basket < ApplicationRecord
   has_many :basket_items, dependent: :destroy
   has_many :items, through: :basket_items
+
+  def total_price
+    total_jpy_price
+    # binding.pry
+  end
+
+  private
+  def total_jpy_price
+    items.sum(:price)
+  end
 end
