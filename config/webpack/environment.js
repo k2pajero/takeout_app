89a1c2282774b369acb84fcb4b2d60a408d84957
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-19 03:45:52 +0900

    Update environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,11 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
+// const webpack = require('webpack')
 
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    jquery: 'jquery',
-  })
-)
+// environment.plugins.prepend('Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery',
+//     jQuery: 'jquery',
+//     jquery: 'jquery',
+//   })
+// )
 module.exports = environment
