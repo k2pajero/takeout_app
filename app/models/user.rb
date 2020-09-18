965935d7430fc4bff1c43abc466c9ca77e2e7a96
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-18 18:36:53 +0900

    トランザクション処理

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,15 +30,19 @@ class User < ApplicationRecord
 
   def checkout!(token, item_ids:)
     total = basket.total_price
+    # total = basket.total_price(item_ids: item_ids)←エラー
 
     transaction do
       basket_items = basket.basket_items.where(item_id: item_ids)
       basket_items.each(&:destroy!)
-
+      # A：買い物かご中身を削除する処理
+      
       purchase_record = prepare_purchase_record
       ids = item_ids.map { |id| { item_id: id } }
       purchase_record.purchase_record_items.create!(ids)
+      # B：購入履歴をレコードに保存する処理
     end
+    # ABの処理をブロックで囲み、１つの処理として扱うトランザクション処理にする
 
     Charge.create!(total, token)
   end
