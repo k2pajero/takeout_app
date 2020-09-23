959ef171dd17bd0edfbdce7eeb6c53263b12603a
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-23 19:26:43 +0900

    モデル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   with_options presence: true do
     validates :nickname
