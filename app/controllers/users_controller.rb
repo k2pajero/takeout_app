1670fbcb6182f7013dc8fb4fae079b133f20b089
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-24 18:56:16 +0900

    登録情報の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def update
     # binding.pry
     if current_user.update(user_params) # 更新出来たかを条件分岐する
+ 
       redirect_to root_path # 更新できたらrootパスへ
     else
       redirect_to "show" # 失敗すれば再度マイページへ
@@ -14,7 +15,8 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :email) # 編集出来る情報を制限
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :real_family_name, :real_first_name, :reading_family_name, :reading_first_name, :birthday, :birthday, :birthday)
+   # 編集出来る情報を制限
   end
   
 end
