3abb097bd698ef19e06804e9bf0227ecd0e0a8e8
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-28 23:01:22 +0900

    adressモデル使用中止

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,46 +12,45 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
+    
+    @user = User.new(sign_up_params)
+    unless @user.valid?
+      render :new and return
+    end 
+    
+    session['devise.regist_data'] = {user: @user.attributes}
+    session['devise.regist_data'][:user]['password'] = params[:user][:password]
+    @address = @user.build_address
+    # render :new_address
+    
     if params[:sns_auth] == 'true'
       pass = Devise.friendly_token
       params[:user][:password] = pass
       params[:user][:password_confirmation] = pass
     end
     super
-    
-    @user = User.new(sign_up_params)
-    unless @user.valid?
-      render :new and return
-    end 
+    # redirect_to root_path 
 
-    session['devise.regist_data'] = {user: @user.attributes}
-    #  binding.pry
-    session['devise.regist_data'][:user]['password'] = params[:user][:password]
-    #  binding.pry
-    @address = @user.build_address
-    render :new_address
   end
-
+  
   def create_address
     @user = User.new(session['devise.regist_data']['user'])
     @address = Address.new(address_params)
-    # binding.pry
-      unless @address.valid?
-        render :new_address 
-      end
+    unless @address.valid?
+      render :new_address 
+    end
     @user.build_address(@address.attributes)
     @user.save
     session['devise.regist_data']['user'].clear
     sign_in(:user, @user)
-    # render root_path
   end
-
-  protected
-
+  
+  private
+  
   def address_params
     params.require(:address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :tell)
   end
-
+  
   # GET /resource/edit
   # def edit
   #   super
