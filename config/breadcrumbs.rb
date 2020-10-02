05d0b1f907c169a408929638d8462fc8e33925e8
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-10-03 06:21:46 +0900

    viewの階層設定

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,6 +2,47 @@ crumb :root do
   link "Home", root_path
 end
 
+crumb :items do
+  link "商品詳細", item_path
+  parent :root
+end
+
+crumb :baskets do
+  link "買い物かご", basket_path
+  parent :root
+end
+
+crumb :users do
+  link "会員情報＆編集", users_path
+  parent :root
+end
+
+crumb :users do
+  link "新規登録", new_user_registration_path
+  parent :root
+end
+
+crumb :listing do
+  link "出品", new_item_path
+  parent :root
+end
+
+crumb :item_edit do
+  link "編集", edit_item_path
+  parent :items
+end
+
+crumb :search do
+  link "検索結果", search_items_path
+  parent :root
+end
+
+crumb :second_search do
+  link "詳細検索結果", second_search_items_path
+  parent :search
+end
+
+
 # crumb :projects do
 #   link "Projects", projects_path
 # end
